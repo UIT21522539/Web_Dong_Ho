7e455e6853054dd2b2312db58c6e3102336e8441
Trần Trúc Quỳnh <96250317+UIT21522539@users.noreply.github.com>
2023-12-01 23:50:38 +0700

    Thêm các request Sản phẩm, Khách hàng, Nhập kho và Đơn hàng

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -11,7 +11,7 @@ class ProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,20 +22,52 @@ class ProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // Cái này đang cài demo thôi chứ đến lúc vào admin mới cài lại
-            'ten'=> 'required|min:5',
-            'email'=> 'required|email|unique:users,email,'.request()->id,
+            'id_brand' => 'required|exists:brand,id_brand',
+            'id_category'=> 'required|exists:category,id_category',
+            'name'=> 'required|min:5',
+            'description'=> 'required|min:5',
+            'sellprice' => 'required|integer|min:10000',
+            'pty_store'=> 'required|min:1',
+            'discount'=> 'required|integer|min:5|max:50',
+            'isdiscount' => 'required|in:0,1',
+            'status'=> 'required|min:5',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
         ];
     }
-    public function messages(): array
-    {
+
+    public function messages(): array{
         return [
-            // Cái này đang cài demo thôi chứ đến lúc vào admin mới cài lại
-            'ten.required'=> 'Họ và tên bắt buộc nhập',
-            'ten.min'=> 'Họ và tên phải từ :min trở lên',
-            'email.required'=> 'Email bắt buộc nhập',
-            'email.email'=> 'Email không đúng định dạng',
-            'email.unique'=> 'Email đã tồn tại',
+            'id_brand.required' => 'Thương hiệu là trường bắt buộc.',
+            'id_brand.exists' => 'Thương hiệu không tồn tại trong cơ sở dữ liệu.',
+
+            'id_category.required' => 'Danh mục là trường bắt buộc.',
+            'id_category.exists' => 'Danh mục không tồn tại trong cơ sở dữ liệu.',
+
+            'name.required' => 'Tên sản phẩm là trường bắt buộc.',
+            'name.min' => 'Tên sản phẩm phải có ít nhất 5 ký tự.',
+
+            'description.required' => 'Mô tả là trường bắt buộc.',
+            'description.min' => 'Mô tả phải có ít nhất 5 ký tự.',
+
+            'sellprice.required' => 'Giá bán là trường bắt buộc.',
+            'sellprice.integer' => 'Giá bán phải là số nguyên.',
+            'sellprice.min' => 'Giá bán phải lớn hơn hoặc bằng 10,000.',
+
+            'pty_store.required' => 'Số lượng tồn kho là trường bắt buộc.',
+            'pty_store.min' => 'Số lượng tồn kho phải lớn hơn 0.',
+
+            'discount.required' => 'Phần trăm giảm giá là trường bắt buộc.',
+            'discount.integer' => 'Phần trăm giảm giá phải là số nguyên.',
+            'discount.min' => 'Phần trăm giảm giá phải lớn hơn hoặc bằng 5.',
+            'discount.max' => 'Phần trăm giảm giá không được lớn hơn 50.',
+
+            'isdiscount.required' => 'Trường isdiscount là bắt buộc.',
+            'isdiscount.in' => 'Trường isdiscount chỉ có thể là 0 hoặc 1.',
+
+            'status.required' => 'Trạng thái là trường bắt buộc.',
+            'status.min' => 'Trạng thái phải có ít nhất 5 ký tự.',
+
+            'image' => 'The :attribute must be an image file (jpeg, png, jpg, gif) and not exceed 2048 kilobytes.',
         ];
     }
 }
