3c34b043182a99a15b145a7b5574224446e677c3
Trần Trúc Quỳnh <96250317+UIT21522539@users.noreply.github.com>
2023-12-01 23:53:10 +0700

    Thêm các route cho admin

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,57 @@ use app\Http\Requests\ProductRequest;
 Route::get('/', [ProductsController::class,'home']);
 Route::get('/home', [ProductsController::class,'home'])->name('product.home');
 
-// Đường dẫn trang danh sách sản phẩm
-Route::get('/product', function () {
-    return view('product');
+
+//Đường dẫn vào admin
+Route::prefix('admin')->group(function () {
+    //Vào trang chủ->Xong
+    Route::get('/', [DashBoardController::class, 'view'])->name('dashboard');
+
+    //Quản lí danh sách đơn hàng->Xong
+    Route::get('/orders', [OrderController::class, 'orderList'])->name('orders.list');
+        //Thông tin chi tiết đơn hàng
+    Route::get('/orders/detail/{id}', [OrderController::class, 'orderDetail'])->name('orders.detail');
+        //Sửa trạng thái sang xác nhận
+    Route::get('/orders/edit/{id}', [OrderController::class, 'updateOrder'])->name('orders.update');
+        //Sửa trạng thái sang đã hủy
+    Route::get('/orders/delete/{id}', [OrderController::class, 'deleteOrder'])->name('orders.delete');
+    
+    //Quản lí danh sách sản phẩm->Chưa xử lí hình ảnh và phần sửa đổi chi tiết
+        //Xem ds
+    Route::get('/products', [ProductController::class, 'productList'])->name('products.list');
+        //Xem chi tiết sản phẩm
+    Route::get('/products/detail/{id}', [ProductController::class, 'productDetail'])->name('products.detail');
+        //Trang hiện thị thêm sản phẩm
+    Route::get('/products/add', [ProductController::class, 'productDetailAdd'])->name('products.detailadd');
+        //Post thêm
+    Route::post('/products/add', [ProductController::class, 'addProduct'])->name('products.add');
+        //Trang hiện thị sửa
+    Route::get('/products/edit/{id}', [ProductController::class, 'getProduct'])->name('products.get');
+        //post update
+    Route::post('/products/update', [ProductController::class, 'updateProduct'])->name('products.update');
+        //delete xóa sản phẩm
+    Route::get('/products/delete/{id}', [ProductController::class, 'deleteProduct'])->name('products.delete');
+        //Tìm kiếm theo tên
+    Route::get('/products/search', [ProductController::class, 'searchProduct'])->name('products.search');
+    
+    //Quản lí danh sách khách hàng -> Chưa có chặn khách hàng
+        //Xem ds
+    Route::get('/users', [UserController::class, 'userList'])->name('users.list');
+        //Xem chi tiết khách hàng
+    Route::get('/users/detail/{id}', [UserController::class, 'userDetail'])->name('users.detail');
+        //Tìm kiếm theo tên
+    Route::get('/users/search', [UserController::class, 'searchUser'])->name('users.search');
+
+    //Quản lí trang danh sách nhập kho
+        //Xem ds nhập kho
+    Route::get('/suppliers', [SupplierController::class, 'supplierList'])->name('suppliers.list');
+        //Xem chi tiết nhập kho
+    Route::get('/suppliers/detail/{id}', [SupplierController::class, 'supplierDetail'])->name('suppliers.detail');
+        //Màn hình thêm phiếu nhập kho
+    Route::get('/suppliers/add', [SupplierController::class, 'supplierDetailAdd'])->name('suppliers.detailadd');    
+        //Thêm
+    Route::get('/suppliers/adddetail', [SupplierController::class, 'addSupplier'])->name('suppliers.add');
+        
 });
 
 
